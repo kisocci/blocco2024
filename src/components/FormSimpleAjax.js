240424a7d2c75dc1fe0ea0024b4d57717c324796
木曽隆 <t-kiso@wevnal.co.jp>
2024-11-14 00:52:29 +0900

    back

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -54,8 +54,6 @@ class Form extends React.Component {
         })
       })
   }
-  document.querySelector("form").addEventListener("submit", handleSubmit);
-  
   render() {
     const { name, subject, action } = this.props
 
