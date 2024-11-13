507bc9e9a66f4ca47def9a859b61a1d0936a8f2a
木曽隆 <t-kiso@wevnal.co.jp>
2024-11-13 22:39:51 +0900

    changed layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -57,8 +57,6 @@ export default ({ children, meta, title }) => {
               titleTemplate={`%s | ${siteTitle}`}
             >
               {title}
-              <link href="https://ucarecdn.com" rel="preconnect" crossorigin />
-              <link rel="dns-prefetch" href="https://ucarecdn.com" />
               {/* Add font link tags here */}
             </Helmet>
 
@@ -72,8 +70,6 @@ export default ({ children, meta, title }) => {
               {...meta}
               {...data.settingsYaml}
             />
-
-            <GithubCorner url="https://github.com/thriveweb/yellowcake" />
 
             <Nav subNav={subNav} />
 
