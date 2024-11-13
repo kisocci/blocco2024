0d25284e19751132c2424ddc899255429fd74e53
木曽隆 <t-kiso@wevnal.co.jp>
2024-11-13 23:22:42 +0900

    fix contact.js

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapPin, Smartphone, Mail } from 'react-feather'
+import { MapPin, Smartphone, Mail, Box } from 'react-feather'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
@@ -16,6 +16,10 @@ export const ContactPageTemplate = ({
   subtitle,
   featuredImage,
   address,
+  address_ja,
+  company,
+  company_ja,
+  establishedIn,
   phone,
   email,
   locations
@@ -31,21 +35,17 @@ export const ContactPageTemplate = ({
         <div>
           <Content source={body} />
           <div className="Contact--Details">
+            <a className="Contact--Details--Item Contact--Details--Cursor" href="javascript:void(0);"><Box />{company_ja}</a>
             {address && (
               <a
                 className="Contact--Details--Item"
                 href={`https://www.google.com.au/maps/search/${encodeURI(
-                  address
+                  address_ja
                 )}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <MapPin /> {address}
-              </a>
-            )}
-            {phone && (
-              <a className="Contact--Details--Item" href={`tel:${phone}`}>
-                <Smartphone /> {phone}
+                <MapPin /> {address_ja}
               </a>
             )}
             {email && (
@@ -53,11 +53,22 @@ export const ContactPageTemplate = ({
                 <Mail /> {email}
               </a>
             )}
+            {phone && (
+              <a className="Contact--Details--Item" href={`tel:${phone}`}>
+                <Smartphone /> {phone} <small>※お電話がとれない場合がございます。<br/>　Eメールでお問い合わせください。</small>
+              </a>
+            )}
+            <hr class="Contact--Details-hr"/>
+            <a className="Contact--Details--Item Contact--Details--Cursor" href="javascript:void(0);"><Box /><b class="Contact--Details--HItem-4">設立</b>2001年12月10日</a>
+            <a className="Contact--Details--Item Contact--Details--Cursor" href="javascript:void(0);"><Box /><b class="Contact--Details--HItem-3">代表者</b>取締役社長　木曽 隆</a>
+            <a className="Contact--Details--Item Contact--Details--Cursor" href="javascript:void(0);"><Box /><b class="Contact--Details--HItem-2">取引銀行</b>沖縄銀行 本店営業部</a>
+            <a className="Contact--Details--Item Contact--Details--Cursor" href="javascript:void(0);"><Box /><b class="Contact--Details--HItem-1">主要取引先</b>琉球放送株式会社、株式会社ハヤテ</a>
+            
           </div>
         </div>
 
         <div>
-          <FormSimpleAjax name="Simple Form Ajax" />
+          <FormSimpleAjax name="bloccoContact" />
         </div>
       </div>
     </section>
@@ -88,6 +99,10 @@ export const pageQuery = graphql`
         subtitle
         featuredImage
         address
+        address_ja
+        company
+        company_ja
+        establishedIn
         phone
         email
         locations {
